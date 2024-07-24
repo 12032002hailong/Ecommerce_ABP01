e24cb38bb6d543588ed145c69938a85c65ee2f27
Long Nguyễn Đức Hải <hailongkc1203@gmail.com>
2024-07-25 01:39:24 +0700

    Migrate data

---
+++ aspnet-core/src/TeduEcommerce.Domain.Shared/Products/ProductType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduEcommerce.Products
+{
+    public enum ProductType
+    {
+        Single,
+        Grouped,
+        Configurable,
+        Bundle,
+        Virtual,
+        Downloadable,
+
+    }
+}
