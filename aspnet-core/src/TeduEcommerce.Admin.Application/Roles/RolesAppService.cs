7d35b47155b25a94db70f2fb11191240b4f3ab6d
Long Nguyễn Đức Hải <hailongkc1203@gmail.com>
2024-10-15 20:48:49 +0700

    Phân quyền cho roles

--- aspnet-core/src/TeduEcommerce.Admin.Application/Roles/RolesAppService.cs
+++ aspnet-core/src/TeduEcommerce.Admin.Application/Roles/RolesAppService.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TeduEcommerce.Admin.ProductCategories;
-using TeduEcommerce.ProductCategories;
 using TeduEcommerce.Roles;
 using Volo.Abp;
 using Volo.Abp.Application.Dtos;
